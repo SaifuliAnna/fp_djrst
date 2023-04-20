b343c638d98ddc7df8dd91da674fc8ece1956083
Saifulina Anna <saifulianna.it@gmail.com>
2023-04-20 20:15:04 +0300

    Initial commit

---
+++ famous_people/women/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Women
+from .serializers import WomenSerializer
+
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        return Response({'title': 'Angelina Jolie'})
+
+
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
