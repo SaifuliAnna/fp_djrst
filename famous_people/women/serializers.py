7e3fb5fdd9257741be506fb6996ae1022581c32c
Saifulina Anna <saifulianna.it@gmail.com>
2023-04-21 11:10:05 +0300

    clear_to_5

--- famous_people/women/serializers.py
+++ famous_people/women/serializers.py
@@ -38,38 +38,3 @@ class WomenSerializer(serializers.Serializer):
         instance.cat_id = validate_data.get("cat_id", instance.cat_id)
         instance.save()
         return instance
-
-
-# 2____________
-# class WomenModel:
-#     def __init__(self, title, content):
-#         self.title = title
-#         self.content = content
-#
-#
-# class WomenSerializer(serializers.Serializer):
-#     title = serializers.CharField(max_length=255)
-#     content = serializers.CharField()
-#
-#
-# def encode():
-#     model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
-#     model_sr = WomenSerializer(model)
-#     print(model_sr.data, type(model_sr), sep='\n')
-#     json = JSONRenderer().render(model_sr.data)
-#     print(json)
-#
-#
-# def decode():
-#     stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Content: Angelina Jolie"}')
-#     data = JSONParser().parse(stream)
-#     serializer = WomenSerializer(data=data)
-#     serializer.is_valid()
-#     print(serializer.validated_data)
-
-
-# 1_________
-# class WomenSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Women
-#         fields = ('title', 'cat_id')
--- famous_people/women/views.py
+++ famous_people/women/views.py
@@ -8,7 +8,7 @@ from .models import Women
 from .serializers import WomenSerializer
 
 
-# 5_________
+# 6_________
 class WomenAPIView(APIView):
     def get(self, request):
         w = Women.objects.all()
@@ -50,53 +50,3 @@ class WomenAPIView(APIView):
 
         return Response({"post": f"delete post {str(pk)}"})
 
-
-# # 4_________
-# class WomenAPIView(APIView):
-#     def get(self, request):
-#         w = Women.objects.all()
-#         return Response({'posts': WomenSerializer(w, many=True).data})
-#
-#     def post(self, request):
-#         serializer = WomenSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#
-#         post_new = Women.objects.create(
-#             title=request.data['title'],
-#             content=request.data['content'],
-#             cat_id=request.data['cat_id']
-#         )
-#
-#         return Response({'post': WomenSerializer(post_new).data})
-#
-
-# 2_________
-# class WomenAPIView(APIView):
-#     def get(self, request):
-#         lst = Women.objects.all()
-#         return Response({'title': 'Angelina Jolie'})
-#
-#     def post(self, request):
-#         return Response({'title': 'Jenife Lopes'})
-
-
-# 3_________
-# class WomenAPIView(APIView):
-#     def get(self, request):
-#         lst = Women.objects.all().values()
-#         return Response({'posts': list(lst)})
-#
-#     def post(self, request):
-#         post_new = Women.objects.create(
-#             title=request.data['title'],
-#             content=request.data['content'],
-#             cat_id=request.data['cat_id']
-#         )
-#
-#         return Response({'post': model_to_dict(post_new)})
-
-
-# 1_________
-# class WomenAPIView(generics.ListAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
