a3879d022af04ecb287ba9d72a08e198bcf934ea
Saifulina Anna <saifulianna.it@gmail.com>
2023-04-21 11:05:41 +0300

    5-les

--- famous_people/women/views.py
+++ famous_people/women/views.py
@@ -8,7 +8,7 @@ from .models import Women
 from .serializers import WomenSerializer
 
 
-# 4_________
+# 5_________
 class WomenAPIView(APIView):
     def get(self, request):
         w = Women.objects.all()
@@ -17,15 +17,58 @@ class WomenAPIView(APIView):
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
+        return Response({'post': serializer.data})
 
-        return Response({'post': WomenSerializer(post_new).data})
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:  # if pk is None:
+           return Response({"error": "Method PUT not allowed"})
 
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exists"})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"post": serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:  # if pk is None:
+           return Response({"error": "Method DELETE not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Object does not exists"})
+
+        instance.delete()
+
+        return Response({"post": f"delete post {str(pk)}"})
+
+
+# # 4_________
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         w = Women.objects.all()
+#         return Response({'posts': WomenSerializer(w, many=True).data})
+#
+#     def post(self, request):
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#
+#         post_new = Women.objects.create(
+#             title=request.data['title'],
+#             content=request.data['content'],
+#             cat_id=request.data['cat_id']
+#         )
+#
+#         return Response({'post': WomenSerializer(post_new).data})
+#
 
 # 2_________
 # class WomenAPIView(APIView):
