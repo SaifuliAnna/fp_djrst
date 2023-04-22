3800bc525116ec480ca4c69a05cee66661f60353
Saifulina Anna <saifulianna.it@gmail.com>
2023-04-22 08:09:23 +0300

    9.1-les

--- famous_people/famous_people/urls.py
+++ famous_people/famous_people/urls.py
@@ -21,11 +21,13 @@ from women.views import *
 from rest_framework import routers
 
 router = routers.SimpleRouter()
+# router = routers.DefaultRouter()
 router.register(r'women', WomenViewSet)
+# print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)), # http://127.0.0.1:8000/api/v1/women/
+    path('api/v1/', include(router.urls)),  # http://127.0.0.1:8000/api/v1/women/
     # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
     # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
 ]
--- famous_people/women/views.py
+++ famous_people/women/views.py
@@ -1,10 +1,11 @@
 from django.forms import model_to_dict
 from rest_framework import generics, viewsets
 from django.shortcuts import render
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Women
+from .models import Women, Category
 from .serializers import WomenSerializer
 
 
@@ -12,3 +13,14 @@ from .serializers import WomenSerializer
 class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
+
+    # @action(methods=['get'], detail=False)
+    # def category(self, request):
+    #     cats = Category.objects.all()
+    #     return Response({'cats': [c.name for c in cats]})
+
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
