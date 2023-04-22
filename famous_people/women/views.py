3a351e44cb3d4503e939c1721604aabcb1a81942
Saifulina Anna <saifulianna.it@gmail.com>
2023-04-22 07:46:18 +0300

    8-les

--- famous_people/women/views.py
+++ famous_people/women/views.py
@@ -1,5 +1,5 @@
 from django.forms import model_to_dict
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,18 +8,7 @@ from .models import Women
 from .serializers import WomenSerializer
 
 
-# 6_________
-class WomenAPIList(generics.ListCreateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-
-
-# 7_________
-class WomenAPIUpdate(generics.UpdateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
-
-
-class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+# 8_________
+class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
