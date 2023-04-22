7e5b4cd8fa1f4f0c7a6d5aa3d0cf35e5160846b2
Saifulina Anna <saifulianna.it@gmail.com>
2023-04-22 21:10:24 +0300

    10.1-les

--- famous_people/women/views.py
+++ famous_people/women/views.py
@@ -2,34 +2,30 @@ from django.forms import model_to_dict
 from rest_framework import generics, viewsets
 from django.shortcuts import render
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Women, Category
+# from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import WomenSerializer
 
 
-# 8_________
-class WomenViewSet(viewsets.ModelViewSet):
-    # queryset = Women.objects.all() якщо 17 строчка є то цієї не потрібно і в юрлсах треба змінити
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
     serializer_class = WomenSerializer
-
-    def get_queryset(self):
-        pk = self.kwargs.get('pk')
-
-        if not pk:
-            return Women.objects.all()[:3]
-
-        return Women.objects.filter(pk=pk)
+    # permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
-    # @action(methods=['get'], detail=False)
-    # def category(self, request):
-    #     cats = Category.objects.all()
-    #     return Response({'cats': [c.name for c in cats]})
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    # permission_classes = (IsOwnerOrReadOnly, )
 
 
-    @action(methods=['get'], detail=True)
-    def category(self, request, pk=None):
-        cats = Category.objects.get(pk=pk)
-        return Response({'cats': cats.name})
+class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    # permission_classes = (IsAdminOrReadOnly, )
+
+
