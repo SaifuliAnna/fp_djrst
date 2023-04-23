b936952e74381b7ad0a54e68cfe348ca766bc174
Saifulina Anna <saifulianna.it@gmail.com>
2023-04-23 21:51:25 +0300

    10.2-les

--- famous_people/women/serializers.py
+++ famous_people/women/serializers.py
@@ -8,7 +8,7 @@ from .models import Women
 
 
 class WomenSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
         model = Women
-        # fields = ("title", "content", "cat")
         fields = "__all__"
--- famous_people/women/views.py
+++ famous_people/women/views.py
@@ -7,25 +7,25 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Women, Category
-# from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import WomenSerializer
 
 
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    # permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    # permission_classes = (IsOwnerOrReadOnly, )
+    permission_classes = (IsOwnerOrReadOnly, )
 
 
 class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    # permission_classes = (IsAdminOrReadOnly, )
+    permission_classes = (IsAdminOrReadOnly, )
 
 
