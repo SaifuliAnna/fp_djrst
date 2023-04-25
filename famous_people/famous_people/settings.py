ce58dd4a5527156fa2631778d3872af86ae4799b
Saifulina Anna <saifulianna.it@gmail.com>
2023-04-25 21:51:49 +0300

    seckret

--- famous_people/famous_people/settings.py
+++ famous_people/famous_people/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@v46wb2snd0)-6@9xd34ikjagod65*6-8vqha+otrf$$w6y9uf'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
